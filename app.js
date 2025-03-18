07a738cdd94f1d43ed415a70192eaf8d85f2794c
Shapeways.io <shapewaysio@Shapewaysios-iMac-2.local>
2025-03-18 09:52:46 +0000

    Implement sticky navbar

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const header = document.querySelector(".header");
 
 function toggleBtnFunction() {
   header.classList.toggle("nav-open");
+  header.classList.toggle("sticky");
 }
 
 menuBtn.addEventListener("click", toggleBtnFunction);

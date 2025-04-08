97f5170e1527b43081d219f8440c8407728a598d
Shapeways.io <shapewaysio@Shapewaysios-iMac-2.local>
2025-04-08 13:05:02 +0000

    Add fourth JavaScript project

---
+++ javascript-proj4/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const clock = document.querySelector(".clock");
+const todaysDate = document.querySelector(".todays-date");
+
+const convertDate = () => {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    weekday: "long",
+  };
+
+  const date = new Intl.DateTimeFormat("en-GB", options).format(new Date());
+
+  todaysDate.innerHTML = date;
+};
+
+const updateClock = () => {
+  const now = new Date();
+
+  let hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+
+  const amPm = hours >= 12 ? "PM" : "AM";
+
+  hours = (hours % 12 || 12).toString().padStart(2, "0");
+
+  clock.textContent = `${hours}:${minutes}:${seconds} ${amPm}`;
+};
+
+setInterval(updateClock, 1000);
+updateClock();
+convertDate();
